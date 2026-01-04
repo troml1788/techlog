78be0a0adcbc214f618d7d8bc81a167b7fb0e213
troml1788 <wgettingrich@gmail.com>
2026-01-04 23:11:56 +0800

    更新-虚拟继电器

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -131,7 +131,7 @@ export default {
                             collapsed: true,    // 如果不加这个，二级菜单就会受父组件影响默认展开，但是无法关闭，仅展示级联效果
                             items: [
                                 { text: "PLC工作原理&工作过程&双线圈冲突", link: "/articles/PLC/learn/inovance/learn/learn2026010401" },
-                                { text: "抢答灯实战", link: "/articles/PLC/learn/inovance/learn/learn2026010302" },
+                                { text: "虚拟继电器M", link: "/articles/PLC/learn/inovance/learn/learn2026010402" },
                                 { text: "置位复位和沿指令", link: "/articles/PLC/learn/siemens/learn007" },
                             ]
                         },
